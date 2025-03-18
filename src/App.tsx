506e850964c621c099d532fe22f748f1304faea5
Dmitriy <66356974+DiamanDD@users.noreply.github.com>
2025-03-18 20:32:45 +0300

    homework2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Homework1 from "./homework/hw1/Homework1.tsx";
+import Homework1 from "./homework/homework1/Homework1.tsx";
+import Homework2 from "./homework/homework2/Homework2.tsx";
 
 
 function App() {
     return (<>
         <Homework1/>
+        <Homework2/>
     </>)
 }
 
